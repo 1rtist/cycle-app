3099f67c6cf60bb932a01ce1ed67f726484161e6
1rtist <stealthetrigger@gmail.com>
2025-09-09 22:29:19 -0700

    g

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
+        
         'vaul@1.1.2': 'vaul',
         'sonner@2.0.3': 'sonner',
         'recharts@2.15.2': 'recharts',
@@ -46,7 +47,7 @@
         '@radix-ui/react-aspect-ratio@1.1.2': '@radix-ui/react-aspect-ratio',
         '@radix-ui/react-alert-dialog@1.1.6': '@radix-ui/react-alert-dialog',
         '@radix-ui/react-accordion@1.2.3': '@radix-ui/react-accordion',
-        '@': path.resolve(__dirname, './src'),
+        "@": path.resolve(__dirname, "src"),
       },
     },
     build: {
